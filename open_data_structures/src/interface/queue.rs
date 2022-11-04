fc236e93ffe09b9dbf69a4be4fa155ffcb3d355e
鷹取敏志 <takatorix@gmail.com>
2022-11-04 12:49:40 +0900

    remove Cargo.lock

--- open_data_structures/src/interface/queue.rs
+++ open_data_structures/src/interface/queue.rs
@@ -10,8 +10,8 @@ pub trait Queue<T> {
 /// 先頭と末尾を持った要素の列を表す
 /// 先頭または末尾に要素を追加できる
 pub trait Deque<T> {
-    fn addFirst(&mut self, x: T);
-    fn removeFirst(&mut self) -> Option<T>;
-    fn addLast(&mut self, x: T);
-    fn removeLast(&mut self) -> Option<T>;
+    fn add_first(&mut self, x: T);
+    fn remove_first(&mut self) -> Option<T>;
+    fn add_last(&mut self, x: T);
+    fn remove_last(&mut self) -> Option<T>;
 }

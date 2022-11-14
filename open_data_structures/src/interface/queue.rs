23c025dc0a84ce3ca0d64579719a3579fff96bef
鷹取敏志 <takatorix@gmail.com>
2022-11-15 00:02:56 +0900

    fix queue interface

--- open_data_structures/src/interface/queue.rs
+++ open_data_structures/src/interface/queue.rs
@@ -1,7 +1,7 @@
 pub trait Queue<T> {
     /// 値xをQueueに追加する
-    fn add(&mut self, x: T);
+    fn enqueue(&mut self, x: T);
 
     /// 以前に追加された「次の値」yをQueueから削除し、yを返す
-    fn remove(&mut self) -> Option<T>;
+    fn dequeue(&mut self) -> Option<T>;
 }

5a7939284a0e4636bce0987e2e8c9cee89534851
鷹取敏志 <takatorix@gmail.com>
2022-12-01 00:17:15 +0900

    refactor sl_list debug

--- open_data_structures/src/data_structure/sl_list.rs
+++ open_data_structures/src/data_structure/sl_list.rs
@@ -1,26 +1,10 @@
+use std::borrow::Borrow;
+use std::fmt::{self, Debug};
 use std::{cell::RefCell, rc::Rc};
 
 use crate::interface::queue::Queue;
 use crate::interface::stack::Stack;
 
-#[derive(Debug)]
-pub struct SLList<T> {
-    head: Option<Rc<RefCell<Node<T>>>>,
-    tail: Option<Rc<RefCell<Node<T>>>>,
-    n: usize,
-}
-
-impl<T> SLList<T> {
-    fn new() -> Self {
-        Self {
-            head: None,
-            tail: None,
-            n: 0,
-        }
-    }
-}
-
-#[derive(Debug)]
 pub struct Node<T> {
     x: T,
     next: Option<Rc<RefCell<Node<T>>>>,
@@ -29,6 +13,50 @@ pub struct Node<T> {
 impl<T> Node<T> {
     fn new(x: T) -> Self {
         Self { x, next: None }
+    }
+}
+
+impl<T: Debug> fmt::Debug for Node<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(n) = &self.next {
+            write!(f, "{:?} -> {:?}", self.x, n.borrow_mut().x)
+        } else {
+            write!(f, "{:?}", self.x)
+        }
+    }
+}
+
+/// Singley-Linked List(単方向連結リスト)
+///
+/// StackとQueueインタフェースを実装する
+/// push(x),pop(),add(x),remove()の実行時間はいずれもO(1)
+///
+/// Dequeの操作もほぼ全て実装できるが、末尾を削除する操作が足りない
+/// 末尾を削除する場合、末尾の一つ前のノードを探す必要があるが
+/// これには、各ノードをheadから順にn-2回辿る必要がある
+pub struct SLList<T> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
+    n: usize,
+}
+
+impl<T> SLList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+            n: 0,
+        }
+    }
+}
+
+impl<T: Debug> fmt::Debug for SLList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(n) = &self.head {
+            write!(f, "haed -> {:?}", n.borrow_mut())
+        } else {
+            write!(f, "null")
+        }
     }
 }
 
@@ -100,6 +128,7 @@ mod tests {
 
         assert_eq!(list.pop(), Some('y'));
         assert_eq!(list.n, 5);
+        println!("{:?}", list);
     }
 
     #[test]

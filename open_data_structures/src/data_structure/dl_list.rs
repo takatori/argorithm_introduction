cf1b668665e2cf1e08b48ab87f89243a02566e4e
鷹取敏志 <takatorix@gmail.com>
2022-12-14 23:35:33 +0900

    refactor

--- open_data_structures/src/data_structure/dl_list.rs
+++ open_data_structures/src/data_structure/dl_list.rs
@@ -1,14 +1,16 @@
-use std::borrow::{Borrow, BorrowMut};
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
 use crate::interface::clone_list::CloneList;
 
+type StrongLink<T> = Rc<RefCell<Node<T>>>;
+type WeakLink<T> = Weak<RefCell<Node<T>>>;
+
 #[derive(Debug)]
 pub struct Node<T> {
     x: T,
-    next: Option<Rc<RefCell<Node<T>>>>,
-    prev: Option<Weak<RefCell<Node<T>>>>,
+    next: Option<StrongLink<T>>,
+    prev: Option<WeakLink<T>>,
 }
 
 impl<T: Default> Node<T> {
@@ -19,6 +21,22 @@ impl<T: Default> Node<T> {
             next: None,
         }
     }
+
+    fn new_link() -> StrongLink<T> {
+        Rc::new(RefCell::new(Self::new()))
+    }
+}
+
+
+pub trait Link<L> {
+    fn new() -> L;
+}
+
+impl<T: Default> Link<StrongLink<T>> for StrongLink<T> {
+
+    fn new() -> StrongLink<T> {
+        Rc::new(RefCell::new(Node::new()))
+    }    
 }
 
 /// 双方向連結リスト
@@ -30,14 +48,14 @@ pub struct DLList<T> {
 
 impl<T: Default + Clone> DLList<T> {
     pub fn new() -> Self {
-        let dummy: Rc<RefCell<Node<T>>> = Rc::new(RefCell::new(Node::new()));
+        let dummy: StrongLink<T> = Node::new_link();
         dummy.as_ref().borrow_mut().next = Some(Rc::clone(&dummy));
         dummy.as_ref().borrow_mut().prev = Some(Rc::downgrade(&dummy));
         Self { dummy, n: 0 }
     }
 
-    pub fn get_node(&self, i: usize) -> Option<Rc<RefCell<Node<T>>>> {
-        let mut p: Option<Rc<RefCell<Node<T>>>>;
+    pub fn get_link(&self, i: usize) -> Option<StrongLink<T>> {
+        let mut p: Option<StrongLink<T>>;
         if i < self.n / 2 {
             p = self.dummy.as_ref().borrow().next.clone();
             for _ in 0..i {
@@ -60,8 +78,8 @@ impl<T: Default + Clone> DLList<T> {
         p
     }
 
-    pub fn add_before(&mut self, w: Option<Rc<RefCell<Node<T>>>>, x: T) -> Rc<RefCell<Node<T>>> {
-        let u = Rc::new(RefCell::new(Node::new()));
+    pub fn add_before(&mut self, w: Option<StrongLink<T>>, x: T) {
+        let u = Node::new_link();
         u.as_ref().borrow_mut().x = x;
         u.as_ref().borrow_mut().prev = w.as_ref().and_then(|rc| rc.as_ref().borrow().prev.clone());
         if let Some(p) = w.as_ref() {
@@ -73,7 +91,6 @@ impl<T: Default + Clone> DLList<T> {
                 .map(|p| p.as_ref().borrow_mut().next = Some(Rc::clone(&u)))
         });
         self.n += 1;
-        u
     }
 
     pub fn remove_node(&mut self, w: Option<Rc<RefCell<Node<T>>>>) {
@@ -94,21 +111,21 @@ impl<T: Default + Clone> CloneList<T> for DLList<T> {
     }
 
     fn get(&self, i: usize) -> Option<T> {
-        self.get_node(i).map(|rc| rc.as_ref().borrow().x.clone())
+        self.get_link(i).map(|rc| rc.as_ref().borrow().x.clone())
     }
 
     fn set(&mut self, i: usize, x: T) -> T {
-        let u = self.get_node(i);
+        let u = self.get_link(i);
         let y = u.map(|rc| std::mem::replace(&mut rc.as_ref().borrow_mut().x, x));
         y.unwrap()
     }
 
     fn add(&mut self, i: usize, x: T) {
-        self.add_before(self.get_node(i), x);
+        self.add_before(self.get_link(i), x);
     }
 
     fn remove(&mut self, i: usize) -> T {
-        let node = self.get_node(i);
+        let node = self.get_link(i);
         let x = node.as_ref().map(|rc| rc.as_ref().borrow().x.clone());
         self.remove_node(node);
         x.unwrap()

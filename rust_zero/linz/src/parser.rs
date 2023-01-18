0c04e3f14ff38f293a86ff7bdcf079a54c327ec9
鷹取敏志 <takatorix@gmail.com>
2023-01-18 23:30:55 +0900

    update

--- rust_zero/linz/src/parser.rs
+++ rust_zero/linz/src/parser.rs
@@ -18,3 +18,88 @@ pub enum Expr {
     Var(String),      // 変数
     QVal(QValExpr),   // 値
 }
+
+/// 関数適用
+#[derive(Debug)]
+pub struct AppExpr {
+    pub expr1: Box<Expr>,
+    pub expr2: Box<Expr>,
+}
+
+/// if式
+#[derive(Debug)]
+pub struct IfExpr {
+    pub cond_expr: Box<Expr>,
+    pub then_expr: Box<Expr>,
+    pub else_expr: Box<Expr>,
+}
+
+/// split式
+#[derive(Debug)]
+pub struct SplitExpr {
+    pub expr: Box<Expr>,
+    pub left: String,
+    pub rigth: String,
+    pub body: Box<Expr>,
+}
+
+/// let式
+#[derive(Debug)]
+pub struct LetExpr {
+    pub var: String,
+    pub ty: TypeExpr,
+    pub expr1: Box<Expr>,
+    pub expr2: Box<Expr>,
+}
+
+/// 値。真偽値、関数、ペア値などになる
+#[derive(Debug)]
+pub enum ValExpr {
+    Bool(bool), // 真偽値リテラル
+    Pair(Box<Expr>, Box<Expr>), // ペア
+    Fun(FnExpr) // 関数(λ抽象)
+}
+
+/// 修飾子
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub emum Qual {
+    Lin, // 線形型
+    Un, // 制約のない一般的な型
+}
+
+/// 修飾子付き値
+#[derive(Debug)]
+pub struct QValExpr {
+    pub qual: Qual,
+    pub val: ValExpr,
+}
+
+/// 関数
+#[derive(Debug)]
+pub struct FnExpr {
+    pub var: String,
+    pub ty: TypeExpr,
+    pub expr: Box<Expr>,
+}
+
+/// free文
+#[derive(Debug)]
+pub struct FreeExpr {
+    pub var: String,
+    pub expr: Box<Expr>,
+}
+
+/// 修飾子付き型
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct TypeExpr {
+    pub qual: Qual,
+    pub prim: PrimType,
+}
+
+/// プリミティブ型
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum PrimType {
+    Bool, // 真偽値型
+    Pair(Box<TypeExpr>, Box<TypeExpr>), // ペア型
+    Arrow(Box<TypeExpr>, Box<TypeExpr>), // 関数型
+}

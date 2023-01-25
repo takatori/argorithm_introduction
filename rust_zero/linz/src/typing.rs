644fb4125e13bc76ecd5981a52d1d412d679e64c
鷹取敏志 <takatorix@gmail.com>
2023-01-25 23:21:25 +0900

    update

--- rust_zero/linz/src/typing.rs
+++ rust_zero/linz/src/typing.rs
@@ -1,1 +1,17 @@
+use crate::{helper::safe_add, parser};
+use std::{borrow::Cow, cmp::Ordering, collections::BTreeMap, mem};
 
+type VarToType = BTreeMap<String, Option<parser::TypeExpr>>;
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+struct TypeEnvStack {
+    vars: BTreeMap<usize, VarToType>,
+}
+
+impl TypeEnvStack {
+    fn new() -> TypeEnvStack {
+        TypeEnvStack {
+            vars: BTreeMap::new(),
+        }
+    }
+}

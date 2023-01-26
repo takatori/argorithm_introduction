e80404679fc19b89aa745c3e869eb95ab8e29964
鷹取敏志 <takatorix@gmail.com>
2023-01-26 22:04:52 +0900

    update linz

--- rust_zero/linz/src/typing.rs
+++ rust_zero/linz/src/typing.rs
@@ -14,4 +14,21 @@ impl TypeEnvStack {
             vars: BTreeMap::new(),
         }
     }
+
+    /// 型環境をpush
+    fn push(&mut self, depth: usize) {
+        self.vars.insert(depth, BTreeMap::new());
+    }
+
+    /// 型環境をpop
+    fn pop(&mut self, depth: usize) -> Option<VarToType> {
+        self.vars.remove(&depth)
+    }
+
+    /// スタックの最も上にある肩環境に変数と型を追加
+    fn insert(&mut self, key: String, value: parser::TypeExpr) {
+        if let Some(last) = self.vars.iter_mut().next_back() {
+            last.1.insert(key, Some(value));
+        }
+    }
 }
